945c3d4c0b2e63910ce692584eb79f5d278994ff
Chaitra-bhat <34712573+Chaitra-bhat@users.noreply.github.com>
2021-12-27 21:30:06 +0530

    spi driver file update

---
+++ DRIVERS_STM32F407VG/WORKSPACE/spi_drivers/Debug/drivers/Src/spi.d
@@ -0,0 +1,7 @@
+drivers/Src/spi.o: ../drivers/Src/spi.c \
+ C:/Users/Navjeevan/OneDrive/Documents/GitHub/baremetalSTM32DISC1/DRIVERS_STM32F407VG/WORKSPACE/spi_drivers/drivers/Inc/spi.h \
+ C:/Users/Navjeevan/OneDrive/Documents/GitHub/baremetalSTM32DISC1/DRIVERS_STM32F407VG/WORKSPACE/spi_drivers/drivers/Inc/stm32f4xx_driver.h
+
+C:/Users/Navjeevan/OneDrive/Documents/GitHub/baremetalSTM32DISC1/DRIVERS_STM32F407VG/WORKSPACE/spi_drivers/drivers/Inc/spi.h:
+
+C:/Users/Navjeevan/OneDrive/Documents/GitHub/baremetalSTM32DISC1/DRIVERS_STM32F407VG/WORKSPACE/spi_drivers/drivers/Inc/stm32f4xx_driver.h:
